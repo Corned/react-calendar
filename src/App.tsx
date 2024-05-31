d9688030ca877e3ac98dd9a83277127947db7ad4
Oliver K <Corned@users.noreply.github.com>
2024-05-31 12:24:51 +0300

    :P

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import "./App.css"
 
+const date = new Date()
+const year = date.getFullYear()
+//const month = date.getMonth() + 1 // JavaScript months are index-based.
+const day = date.getDate()
+const dayOfWeek = date.getDay()
+
 const Calendar = () => {
-  const date = new Date()
-  const year = date.getFullYear()
-  const month = date.getMonth()
-  const day = date.getDate()
-  const dayOfWeek = date.getDay()
+  const [ month, setMonth ] = useState(date.getMonth() + 1)
+
+  const rows = 6
+  const cols = 7
 
   console.log({ year, month });
 
@@ -15,33 +21,44 @@ const Calendar = () => {
   ]
 
   const getNumberOfDaysInMonth = (year, month) =>
-    new Date(year, month + 1, 0).getDate()
+    new Date(year, month, 0).getDate()
 
   const getFirstDayOfMonth = (year, month) =>
-    new Date(year, month, 1).getDay()
+    new Date(year, month - 1, 1).getDay()
 
   const getLastDayOfMonth = (year, month) =>
     new Date(year, month, 0).getDay()
-
   
-    
-  console.log( new Date(year, month + 1, 0).getDay() )
-  console.log( new Date(year, month + 1, 0).getMonth() )
-
-
-  
+  const dateString = date.toLocaleDateString()
   const daysInCurrentMonth = getNumberOfDaysInMonth(year, month)
   const firstDayOfCurrentMonth = days[getFirstDayOfMonth(year, month)]
   const lastDayOfCurrentMonth = days[getLastDayOfMonth(year, month)]
 
-  console.log({ daysInCurrentMonth, firstDayOfCurrentMonth, lastDayOfCurrentMonth });
-  
+  console.log({ dateString, daysInCurrentMonth, firstDayOfCurrentMonth, lastDayOfCurrentMonth });
+
+  const daysBefore = getFirstDayOfMonth(year, month)  
     
+  const items = []
+
+  while (items.length < getFirstDayOfMonth(year, month)) {
+    items.push({ x: -1 })
+  }
+
+
+
+  for (let i = 0; i < daysInCurrentMonth; i++) {
+    items.push({ x: i  + 1 })
+  }
+
+
+  while (items.length < rows * cols) {
+    items.push({ x: -1 })
+  }
 
 
   return (
     <div className="calendar">
-      <p>{ year }, { month }, { day }, { dayOfWeek }</p>
+      <p>{ date.toLocaleDateString() }</p>
 
       <div className="calendar__header">
         <div className="calendar__header-title">
@@ -68,9 +85,20 @@ const Calendar = () => {
       </div>
       <div className="calendar__days">
 
-
+        {
+          items.map((a) => {
+            return (
+              <div className={a.x < 0 ? "grayed" : "normal"}>
+                <p>{ a.x }</p>
+              </div>
+            )
+          })
+        }
 
       </div>
+
+      <button onClick={() => setMonth(month - 1)}>{"<-"}</button>
+      <button onClick={() => setMonth(month + 1)}>{"->"}</button>
 
     </div>
   )
