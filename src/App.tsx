640569ed8cb2ce4711fede7e6aa99cbfb94e48f1
Corned <Corned@users.noreply.github.com>
2024-06-02 22:25:01 +0300

    style + moment based

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,104 @@
 import { useState } from "react"
 import "./App.css"
 
-const date = new Date()
-const year = date.getFullYear()
-//const month = date.getMonth() + 1 // JavaScript months are index-based.
-const day = date.getDate()
-const dayOfWeek = date.getDay()
+import moment from "moment"
 
 const Calendar = () => {
-  const [ month, setMonth ] = useState(date.getMonth() + 1)
+  const [ date, setDate ] = useState(moment())
 
-  const rows = 6
-  const cols = 7
+  const addMonth = () => {
+    setDate((oldDate) => oldDate.clone().add({ month: 1 }))
+  }
 
-  console.log({ year, month });
+  const removeMonth = () => {
+    setDate((oldDate) => oldDate.clone().subtract({ month: 1 }))
+  }
+
 
   const days = [
-    "Sunday", "Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday"
+    "Su", "Ma", "Ti", "Ke",
+    "To", "Pe", "La",
   ]
 
-  const getNumberOfDaysInMonth = (year, month) =>
-    new Date(year, month, 0).getDate()
+  const columns = 7
+  const rows = 6
 
-  const getFirstDayOfMonth = (year, month) =>
-    new Date(year, month - 1, 1).getDay()
 
-  const getLastDayOfMonth = (year, month) =>
-    new Date(year, month, 0).getDay()
-  
-  const dateString = date.toLocaleDateString()
-  const daysInCurrentMonth = getNumberOfDaysInMonth(year, month)
-  const firstDayOfCurrentMonth = days[getFirstDayOfMonth(year, month)]
-  const lastDayOfCurrentMonth = days[getLastDayOfMonth(year, month)]
-
-  console.log({ dateString, daysInCurrentMonth, firstDayOfCurrentMonth, lastDayOfCurrentMonth });
-
-  const daysBefore = getFirstDayOfMonth(year, month)  
-    
   const items = []
 
-  while (items.length < getFirstDayOfMonth(year, month)) {
-    items.push({ x: -1 })
+  // 
+
+
+  const lastMonth = date.clone().subtract({ month: 1 })
+  const daysInLastMonth = lastMonth.daysInMonth()
+
+  const nextMonth = date.clone().subtract({ month: 1 })
+  const daysInNextMonth = nextMonth.daysInMonth()
+  
+  const firstDayOfCurrentMonth = moment([ date.year(), date.month(), 1 ]).day()
+  
+
+
+  for (let dayNumber = (daysInLastMonth - firstDayOfCurrentMonth); dayNumber < daysInLastMonth; dayNumber++) {
+    items.push({
+      dayNumber: dayNumber + 1,
+      current: false,
+    })
   }
 
-
-
-  for (let i = 0; i < daysInCurrentMonth; i++) {
-    items.push({ x: i  + 1 })
+  for (let dayNumber = 1; dayNumber <= date.daysInMonth(); dayNumber++) {
+    items.push({
+      dayNumber,
+      currentMonth: true,
+    })
+  }
+  
+  let i = 0
+  while (items.length < rows * columns) {
+    i++
+    items.push({
+      dayNumber: i,
+      currentMonth: false,
+    })
   }
 
-
-  while (items.length < rows * cols) {
-    items.push({ x: -1 })
-  }
 
 
   return (
-    <div className="calendar">
-      <p>{ date.toLocaleDateString() }</p>
 
-      <div className="calendar__header">
-        <div className="calendar__header-title">
-          <p>Sunday</p>
-        </div>
-        <div className="calendar__header-title">
-          <p>Monday</p>
-        </div>
-        <div className="calendar__header-title">
-          <p>Tuesday</p>
-        </div>
-        <div className="calendar__header-title">
-          <p>Wednesday</p>
-        </div>
-        <div className="calendar__header-title">
-          <p>Thursday</p>
-        </div>
-        <div className="calendar__header-title">
-          <p>Friday</p>
-        </div>
-        <div className="calendar__header-title">
-          <p>Saturday</p>
-        </div>
-      </div>
-      <div className="calendar__days">
+    <>
+      
+      <p>{ date.format("MM/YYYY") }</p>
 
-        {
-          items.map((a) => {
-            return (
-              <div className={a.x < 0 ? "grayed" : "normal"}>
-                <p>{ a.x }</p>
+      <button onClick={removeMonth}>{"<-"}</button>
+      <button onClick={addMonth}>{"->"}</button>
+      
+
+      <div className="calendar">
+
+
+
+        <div className="calendar__header">
+          {
+            days.map((day) => (
+              <div className="calendar__header-title">
+                <p>{ day }</p>
               </div>
-            )
-          })
-        }
+            ))
+          }
+        </div>
+        <div className="calendar__days">
+          {
+            items.map((item) => (
+              <div className={`calendar__day ${item.currentMonth ? "" : "grayed"}`}>
+                <p className="day__number">{ item.dayNumber.toString().padStart(2, "0") }</p>
+              </div>
+            ))
+          }
+        </div>
 
       </div>
-
-      <button onClick={() => setMonth(month - 1)}>{"<-"}</button>
-      <button onClick={() => setMonth(month + 1)}>{"->"}</button>
-
-    </div>
+    </>
   )
 }
 
