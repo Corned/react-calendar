934e527b3b683c2c3b7a700d95cd7056d436b395
Oliver K <Corned@users.noreply.github.com>
2024-06-11 14:33:42 +0300

    slight style changes

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,16 @@ const Popup = ({ close }) => {
   return (
     <div className="popup">
       <div className="popup__header">
-        <div onClick={handleClose}>X</div>
+
+        <svg className="cross" onClick={handleClose} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M18 6L6 18" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M6 6L18 18" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+
+
+
+
       </div>
 
       <div className="popup__container">
